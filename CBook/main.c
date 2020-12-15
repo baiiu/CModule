da79e2bbe9ad3f2b775d55ebead6ee0ba14ce0f2
baiiu <baiiu@foxmail.com>
2020-12-15 23:24:38 +0800

    first

---
+++ CBook/main.c
@@ -0,0 +1,18 @@
+//
+// Created by zhezhu on 2020/12/15.
+//
+
+#include <printf.h>
+#include "test.h"
+
+int main() {
+
+    printf("===========start===========\n");
+
+    hello();
+
+    printf("\n");
+    printf("===========end===========\n");
+
+    return 0;
+}
---
+++ CBook/test.h
@@ -0,0 +1,6 @@
+#ifndef CBOOK_TEST_H
+#define CBOOK_TEST_H
+
+void hello(void);
+
+#endif //CBOOK_TEST_H
